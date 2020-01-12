f8aac153e13bca691f43a1a041b8cea4c0607d9e
sakata.tomoyuki <a1819ts@aiit.ac.jp>
2020-01-13 00:14:22 +0900

    practice

---
+++ practice/practice_a.java
@@ -0,0 +1,14 @@
+// https://atcoder.jp/contests/practice/submissions/9470757
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        String s = sc.next();
+        System.out.println((a + b + c) + " " + s);
+    }
+}
