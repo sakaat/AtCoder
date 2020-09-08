e5748c5af183d73daeaa221e8cc28bc0fc5e97da
sakata.tomoyuki <ohagiyahagi@gmail.com>
2020-09-08 20:35:08 +0900

    practice

---
+++ practice/practice_a.go
@@ -0,0 +1,40 @@
+// https://atcoder.jp/contests/practice/submissions/16579012
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	initialBufSize = 1e4
+	maxBufSize     = 1e8
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	buf := make([]byte, initialBufSize)
+	sc.Buffer(buf, maxBufSize)
+	sc.Split(bufio.ScanWords)
+}
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	a, _ := strconv.Atoi(next())
+	return a
+}
+
+func main() {
+	var a = nextInt()
+	var b, c = nextInt(), nextInt()
+	var s = next()
+	fmt.Println(a+b+c, s)
+}
